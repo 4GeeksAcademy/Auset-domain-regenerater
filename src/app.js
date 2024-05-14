37b66166bd4ebfbd160d05ca585664e487060817
settyy <165927672+settyy@users.noreply.github.com>
2024-05-14 21:30:17 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import "./index.html";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generateRandomDomain() {
+  let pronouns = ["The", "My ", "our", "how"];
+  let adjectives = ["great", "big", "crushed", "broke"];
+  let nouns = ["homework", " phone", "car"];
+  let ends = [".com", ".net", ".org", ".us"];
+  const domainNames = [];
+  for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+      for (const noun of nouns) {
+        for (const end of ends) {
+          const domainName = `${pronoun}${adjective}${noun}${end}`;
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+
+  return domainNames;
+}
+window.onload = () => {
+  const button = document.querySelector("#domainbutton");
+  const p = document.querySelector("#domain");
+  button.onclick = () => {
+    p.innerHTML = generateRandomDomain();
+  };
 };
